de206d09c2aa1912041c85f3d06993ee99580b1a
Eugenio Sobrevilla <esobrevilla@gmail.com>
2017-12-25 23:39:34 -0600

    val

--- hello1.js
+++ hello1.js
@@ -1,5 +1,5 @@
 function saluda(){
-    var n = document.getElementById("nombre").innerHTML;
+    var n = document.getElementById("nombre").value;
     document.getElementById("saludo").innerHTML="Hola" + n;
     //document.write("aquiiii");
 }
